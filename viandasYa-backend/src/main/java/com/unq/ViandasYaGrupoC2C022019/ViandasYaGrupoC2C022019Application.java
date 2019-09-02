8defd40fd7d597fde725c3a5c4f4d4b2ba0ce0c3
sergio <sergioluke35@gmail.com>
2019-09-02 18:07:29 +0200

    proyecto base

---
+++ viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/ViandasYaGrupoC2C022019Application.java
@@ -0,0 +1,13 @@
+package com.unq.ViandasYaGrupoC2C022019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViandasYaGrupoC2C022019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViandasYaGrupoC2C022019Application.class, args);
+	}
+
+}
---
+++ viandasYa-backend/src/test/java/com/unq/ViandasYaGrupoC2C022019/ViandasYaGrupoC2C022019ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unq.ViandasYaGrupoC2C022019;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViandasYaGrupoC2C022019ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
