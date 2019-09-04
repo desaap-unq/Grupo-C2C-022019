80eff76f904bb703f98696fbc69858bce24d4350
sergio <sergioluke35@gmail.com>
2019-09-04 08:27:15 +0200

    estructura del proyecto con test de ejemplo

---
+++ viandasYa-backend/src/test/java/com/unq/ViandasYaGrupoC2C022019/service/UsuarioServiceTest.java
@@ -0,0 +1,22 @@
+package com.unq.ViandasYaGrupoC2C022019.service;
+
+import com.unq.ViandasYaGrupoC2C022019.ViandasYaGrupoC2C022019ApplicationTests;
+import com.unq.ViandasYaGrupoC2C022019.model.Usuario;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UsuarioServiceTest extends ViandasYaGrupoC2C022019ApplicationTests {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Test
+    public void obtenerTodos_conUsuariosPersistidos_retornaUsuarios() {
+        List<Usuario> listaDeUsuarios = usuarioService.obtenerTodos();
+        assertThat(listaDeUsuarios)
+                .asList().isNotEmpty()
+                .hasSize(3);
+    }
+}
