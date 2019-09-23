0bee6724f1f607a32a4248ed6935a4d0a42b44e4
gguzman89 <ggtpi0gabriel@gmail.com>
2019-09-23 20:35:13 -0300

    build herokuapp

--- viandasya-frontend/src/App.js
+++ viandasya-frontend/src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
         <InfoWindow
           lat={coords.lat}
           lng={coords.lng}
-          content={'React + Google :)'}
+          content={'Travis + Google :)'}
           onCloseClick={this.onCloseClick} />
         <Circle
           lat={coords.lat}
