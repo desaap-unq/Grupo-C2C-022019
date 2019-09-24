8be3df43d8fd3d6aeeceaeaef91d29e5eee90ac7
fuser1988 <sergioluke35@gmail.com>
2019-09-24 00:11:28 -0300

    add order test

--- viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/model/Order.java
+++ viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/model/Order.java
@@ -1,20 +1,35 @@
 
 package com.unq.ViandasYaGrupoC2C022019.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 class Order {
     
+    private Client client;
     private Business business;
     private List<Menu> menus = new ArrayList<Menu>();
-    private Client client;
-    private int calification = 0;
+    private DispatchType dispatchType;
+    private LocalDate orderDate;
+    private LocalDate dispatchDate;
+    private OrederState state;
+    private LocalTime dispatchTime;
+    private LocalTime deliveryTime;
+    private int score = 0;
     
-    Order(Client aClient, Business aBusiness, List<Menu> menus) {
-        this.business = aBusiness;
-        this.client = aClient;
+
+    public Order(Client client, Business business, List<Menu> menus, DispatchType dispatchType, LocalDate dispatchDate, LocalTime deliveryTime) {
+        this.client = client;
+        this.business = business;
         this.menus = menus;
+        this.dispatchType = dispatchType;
+        this.dispatchDate = dispatchDate;
+        this.orderDate = LocalDate.now();
+        this.state = OrederState.PROGRESS;
+        this.deliveryTime = deliveryTime;
     }
 
     public Business getBusiness() {
@@ -33,14 +48,14 @@ class Order {
         this.menus = menus;
     }
 
-    public int getCalification() {
-        return calification;
+    public int getScore() {
+        return score;
     }
 
-    public void setCalification(int calification) {
-        this.calification = calification;
+    public void setScore(int score) {
+        this.score = score;
     }
-
+    
     public Client getClient() {
         return client;
     }
@@ -49,6 +64,60 @@ class Order {
         this.client = client;
     }
 
-    
+    public DispatchType getDispatchType() {
+        return dispatchType;
+    }
+
+    public void setDispatchType(DispatchType dispatchType) {
+        this.dispatchType = dispatchType;
+    }
+
+    public LocalDate getDispatchDate() {
+        return dispatchDate;
+    }
+
+    public void setDispatchDate(LocalDate dispatchDate) {
+        this.dispatchDate = dispatchDate;
+    }
+
+    public LocalTime getDispatchTime() {
+        return dispatchTime;
+    }
+
+    public void setDispatchTime(LocalTime dispatchTime) {
+        this.dispatchTime = dispatchTime;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrederState getState() {
+        return state;
+    }
+
+    public void setState(OrederState state) {
+        this.state = state;
+    }
+
+    public boolean hasQualification() {
+        return this.getScore() != 0; 
+    }
+
+    public LocalTime calculateDeliveryTime(){
+        LocalTime deliveryTime = this.menus.stream()
+                .map(menu -> menu.getAverageDeliveryTime())
+                .reduce(LocalTime.of(0,0),(t,t2)-> t.plus(t2.getHour(),ChronoUnit.HOURS)
+                .plusMinutes(t2.getMinute()));
+        if (this.dispatchType == DispatchType.BUSINESS)
+            return deliveryTime;
+        else
+            return deliveryTime.plusHours(this.deliveryTime.getHour()).plusMinutes(this.deliveryTime.getMinute());
+        
+    }
     
 }
