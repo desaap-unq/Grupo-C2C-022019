ef6cc50e774f26ba0a2fe5d64b259cec5c1df449
sergio <sergioluke35@gmail.com>
2019-09-24 09:00:09 +0200

    modification cadacy error

--- viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/model/OrderBuilder.java
+++ viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/model/OrderBuilder.java
@@ -8,58 +8,53 @@ import java.util.List;
 
 public class OrderBuilder {
     
-    public static OrderBuilder aOrder() {
-        return new OrderBuilder();
-    }
-
     private Client client = new Client("Gabriel", "Guzman", "gabriel.guzman@alu.unq.edu.ar", 1515151515, 
                                     "Banfield", "Laprida 458");
     private Business business = new Business("Empanadas Dorita", "logo", "Lanus", "Av San Martin 3192", 
                                           "Google Use", "Empandas de todo tipo", "facebook", "no-resp@gmail.com",
                                           151515, "10 - 18", "lunes a viernes", "1km cerca de la estacion");
-    private List<Menu> menus = new ArrayList<Menu>();
+    private List<Menu> menus = new ArrayList();
     private DispatchType dispatchType = DispatchType.BUSINESS ;
     private LocalDate dispatchDate = LocalDate.now();
-    private LocalTime dispatchTime = LocalTime.now();
-    private LocalTime deliveryTime = LocalTime.of(0,20);;
+    private LocalTime deliveryTime = LocalTime.of(0,20);
+    
+    public static OrderBuilder aOrder() {
+        return new OrderBuilder();
+    }
+    
+    public Order build() {
+	Order aOrder = new Order(client, business, menus, dispatchType, dispatchDate,deliveryTime);
+	return aOrder;
+    }
 	
-	public Order build() {
-		Order aOrder = new Order(client, business, menus, dispatchType, dispatchDate,deliveryTime);
-		return aOrder;
-	}
-	
-	public OrderBuilder withMenus(List<Menu> menus) {
-		this.menus = menus;
-		return this;
-	}
+    public OrderBuilder withMenus(List<Menu> menus) {
+	this.menus = menus;
+	return this;
+    }
         
-        public OrderBuilder withBusiness(Business abusiness) {
-		this.business = abusiness;
-		return this;
-	}
+    public OrderBuilder withBusiness(Business abusiness) {
+	this.business = abusiness;
+	return this;
+    }
         
-        public OrderBuilder withClient(Client aClient) {
-		this.client = aClient;
-		return this;
-	}
+    public OrderBuilder withClient(Client aClient) {
+	this.client = aClient;
+	return this;
+    }
         
-        public OrderBuilder withDispatchType(DispatchType aDispatchType) {
-		this.dispatchType = aDispatchType;
-		return this;
-	}
+    public OrderBuilder withDispatchType(DispatchType aDispatchType) {
+	this.dispatchType = aDispatchType;
+	return this;
+    }
         
-        public OrderBuilder withDispatchDate(LocalDate aDispatchDate) {
-		this.dispatchDate = aDispatchDate;
-		return this;
-	}
+    public OrderBuilder withDispatchDate(LocalDate aDispatchDate) {
+	this.dispatchDate = aDispatchDate;
+	return this;
+    }
         
-        public OrderBuilder withDispatchTime(LocalTime aDispatchTime) {
-		this.dispatchTime = aDispatchTime;
-		return this;
-	}
-        public OrderBuilder withDeliveryTime(LocalTime aDeliveryTime) {
-		this.deliveryTime = aDeliveryTime;
-		return this;
-	}
+    public OrderBuilder withDeliveryTime(LocalTime aDeliveryTime) {
+	this.deliveryTime = aDeliveryTime;
+	return this;
+    }
         
 }
