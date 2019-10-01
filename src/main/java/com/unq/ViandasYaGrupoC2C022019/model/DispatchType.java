cd15abf27cd82cec88d97c2c02c130d2bc627022
fuser1988 <sergioluke35@gmail.com>
2019-10-01 14:16:58 +0200

    Revert "Feature/integration jacoco codacy"

--- src/main/java/com/unq/ViandasYaGrupoC2C022019/model/DispatchType.java
+++
@@ -1,5 +0,0 @@
-package com.unq.ViandasYaGrupoC2C022019.model;
-
-enum DispatchType {
-    DELIVERY,BUSINESS
-}
---
+++ viandasYa-backend/src/main/java/com/unq/ViandasYaGrupoC2C022019/model/Usuario.java
@@ -0,0 +1,55 @@
+package com.unq.ViandasYaGrupoC2C022019.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+    private String apellido;
+    private LocalDateTime fechaDeNacimiento;
+    
+    public Usuario(){}
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDateTime getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(LocalDateTime fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+    
+    
+}
+
