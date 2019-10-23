6d47308d7d1679e9e5b85d31458ddbfa46c9bca8
sergio <sergioluke35@gmail.com>
2019-10-23 15:36:41 +0200

    changes in repository

--- src/main/java/com/unq/ViandasYaGrupoC2C022019/service/BusinessService.java
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/service/BusinessService.java
@@ -1,9 +1,10 @@
 package com.unq.ViandasYaGrupoC2C022019.service;
 
 import com.unq.ViandasYaGrupoC2C022019.model.Business;
+import com.unq.ViandasYaGrupoC2C022019.model.MenuCategory;
 import com.unq.ViandasYaGrupoC2C022019.persistence.BusinessRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -23,5 +24,9 @@ public class BusinessService {
     public Business findBusinessById(Long businessId) {
         Assert.isTrue(businessRepository.existsById(businessId), ("Not found Busness with id ".concat(businessId.toString())));
         return this.businessRepository.findById(businessId).get();
+    }
+
+    public List<Business> findByCategory(MenuCategory menuCategory) {
+        return this.businessRepository.findByMenuCategory(menuCategory);
     }
 }
--- src/test/java/com/unq/ViandasYaGrupoC2C022019/service/BusinessServiceTest.java
+++ src/test/java/com/unq/ViandasYaGrupoC2C022019/service/BusinessServiceTest.java
@@ -1,14 +1,20 @@
-
 package com.unq.ViandasYaGrupoC2C022019.service;
 
 import com.unq.ViandasYaGrupoC2C022019.ApplicationTests;
 import com.unq.ViandasYaGrupoC2C022019.model.Business;
+import com.unq.ViandasYaGrupoC2C022019.model.Menu;
+import com.unq.ViandasYaGrupoC2C022019.model.MenuCategory;
 import com.unq.ViandasYaGrupoC2C022019.model.VirtualWallet;
 import com.unq.ViandasYaGrupoC2C022019.util.BusinessBuilder;
 import com.unq.ViandasYaGrupoC2C022019.util.VirtualWalletBuilder;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,35 +22,57 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class BusinessServiceTest extends ApplicationTests {
-    
+
     @Autowired
     private BusinessService businessService;
     
     @Autowired
+    private MenuService menuService;
+
+    @Autowired
     EntityManager entityManager;
-    
+
     @Test
     public void createBussines_WithBasicData_ABusinessWithIdNotNull() {
         VirtualWallet virtualWallet = VirtualWalletBuilder.aVirtualWallet().buildAndSave(entityManager);
         Business business = BusinessBuilder.aBusiness().withWallet(virtualWallet).build();
-        
+
         businessService.save(business);
         assertNotNull(business.getId());
     }
-    
+
     @Test
     public void findBusinessById_WithBusinessInDataBase_ABusiness() {
         VirtualWallet virtualWallet = VirtualWalletBuilder.aVirtualWallet().buildAndSave(entityManager);
         Business businessSaved = BusinessBuilder.aBusiness().withWallet(virtualWallet).buildAndSave(entityManager);
-        
+
         assertThat(businessService.findBusinessById(businessSaved.getId()).getId());
-        
+
     }
+
     @Test
     public void findBusinessById_WithoutBusinessInDataBase_aException() {
-        
+
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> businessService.findBusinessById(1L))
                 .withMessage("Not found Busness with id 1");
     }
+
+    @Test
+    public void testFindByCategory_Menu() {
+        List<MenuCategory> menuCategoryList = new ArrayList<>();
+        menuCategoryList.add(MenuCategory.HAMBURGUESAS);
+        menuCategoryList.add(MenuCategory.EMPANADAS);
+
+        VirtualWallet virtualWallet = VirtualWalletBuilder.aVirtualWallet().buildAndSave(entityManager);
+
+        Business business = BusinessBuilder.aBusiness().withWallet(virtualWallet).buildAndSave(entityManager);
+
+        Menu menu = new Menu("Hamburguesas", "Hamburguesa de carne con tomate y queso",
+        menuCategoryList, 40D, LocalDate.now(), LocalDate.now(),
+        LocalTime.of(0, 20), LocalTime.of(0, 20), 50D, 1, 50D, 3, 120D, 100, business);
+        menuService.save(menu);
+        assertFalse(businessService.findByCategory(MenuCategory.HAMBURGUESAS).isEmpty());  
+    }
+
 }
