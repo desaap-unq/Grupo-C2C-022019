5136603cef23fc4cdae6e602016e28d123656941
sergio <sergioluke35@gmail.com>
2019-10-28 17:43:35 +0100

    add Order controller

---
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/OrderController.java
@@ -0,0 +1,31 @@
+package com.unq.ViandasYaGrupoC2C022019.webservice;
+
+import com.unq.ViandasYaGrupoC2C022019.dto.OrderDto;
+import com.unq.ViandasYaGrupoC2C022019.model.Order;
+import com.unq.ViandasYaGrupoC2C022019.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("order")
+@CrossOrigin(origins = "http://localhost:3000")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("")
+    public Order createOrder(@RequestBody OrderDto orderDto) {
+        orderDto.print();
+        Order newOrder = new Order();
+        
+//        return orderService.save(newOrder);
+        return newOrder;
+    }
+  
+}
