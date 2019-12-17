2ad0019e0229c4afe2d05590a55b9a077209c04c
gguzman89 <ggtpi0gabriel@gmail.com>
2019-12-17 19:28:13 -0300

    fix error pck methods

--- src/main/java/com/unq/ViandasYaGrupoC2C022019/aspects/LogExecutionTimeAspectCustomPointcut.java
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/aspects/LogExecutionTimeAspectCustomPointcut.java
@@ -1,6 +1,8 @@
 package com.unq.ViandasYaGrupoC2C022019.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,15 +16,17 @@ public class LogExecutionTimeAspectCustomPointcut {
 	
 	static Logger logger = LoggerFactory.getLogger(LogExecutionTimeAspectCustomPointcut.class);
 	
-	@Pointcut("execution(* com.unq.ViandasYaGrupoC2C022019.webservice.*(..))")
+	@Pointcut("execution(* com.unq.ViandasYaGrupoC2C022019.webservice.MenuController.*(..))")
 	public void methodsStarterServicePointcut() {
 		
 	}
 	
+	@Before("methodsStarterServicePointcut()")
 	public void beforeMethods() throws Throwable {
 		logger.info("/////// BEFORE /////");
 	}
 	
+	@After("methodsStarterServicePointcut()")
 	public void afterMethods() throws Throwable {
 		logger.info("/////// AFTER /////");
 	}
--- src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/MenuController.java
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/MenuController.java
@@ -1,11 +1,16 @@
 package com.unq.ViandasYaGrupoC2C022019.webservice;
 
+import com.unq.ViandasYaGrupoC2C022019.aspects.LogExecutionTimeAspectCustomPointcut;
 import com.unq.ViandasYaGrupoC2C022019.model.Business;
 import com.unq.ViandasYaGrupoC2C022019.model.Menu;
 import com.unq.ViandasYaGrupoC2C022019.service.BusinessService;
 import com.unq.ViandasYaGrupoC2C022019.service.MenuService;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("menu")
 @CrossOrigin(origins = "http://localhost:3000")
+@EnableAutoConfiguration
 public class MenuController {
 
+	static Logger logger = LoggerFactory.getLogger(LogExecutionTimeAspectCustomPointcut.class);
+	
     @Autowired
     MenuService menuService;
     
@@ -28,6 +36,7 @@ public class MenuController {
     @GetMapping("/business/{id}")
     public List<Menu> findByBusinessId(@PathVariable long id) {
         List<Menu> menus = menuService.findByBusinessId(id);
+        logger.info("/////// Inside method2() method");
         return menus;
     }
     
