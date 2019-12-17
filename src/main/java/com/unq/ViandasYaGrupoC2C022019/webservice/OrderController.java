c82d029774cdde37ca50de2b835a737cdb43312b
gguzman89 <ggtpi0gabriel@gmail.com>
2019-12-17 20:17:30 -0300

    by time order annotation

--- src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/OrderController.java
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/OrderController.java
@@ -1,5 +1,6 @@
 package com.unq.ViandasYaGrupoC2C022019.webservice;
 
+import com.unq.ViandasYaGrupoC2C022019.aspects.LogExecutionTime;
 import com.unq.ViandasYaGrupoC2C022019.dto.OrderDto;
 import com.unq.ViandasYaGrupoC2C022019.model.Order;
 import com.unq.ViandasYaGrupoC2C022019.service.OrderService;
@@ -19,6 +20,7 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @LogExecutionTime
     @PostMapping("")
     public Order createOrder(@RequestBody OrderDto orderDto) {
         
