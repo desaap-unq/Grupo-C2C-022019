2e3503fea141de69d0d2005071933d9bc29a5826
gguzman89 <ggtpi0gabriel@gmail.com>
2019-12-19 18:28:14 -0300

    fix cors policy

--- src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/ClientController.java
+++ src/main/java/com/unq/ViandasYaGrupoC2C022019/webservice/ClientController.java
@@ -1,6 +1,8 @@
 package com.unq.ViandasYaGrupoC2C022019.webservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,13 @@ import com.unq.ViandasYaGrupoC2C022019.service.ClientService;
 
 @RestController
 @RequestMapping("client")
+@CrossOrigin(origins = "http://localhost:3000")
+@EnableAutoConfiguration
 public class ClientController {
 
 	@Autowired
     ClientService clientService;
 
-    
     @GetMapping("/{id}")
     public Client findById(@PathVariable long id) {
         Client client;
